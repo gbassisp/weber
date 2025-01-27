7977e4fc49e4deaa4a88a5e8ac37e69ee6b6c3c8
guilherme <guilherme.bassi.sp@gmail.com>
2025-01-27 11:55:36 +1030

    feat(integration): added macro test usage

--- weber-integration/bin/controllers/controller1.dart
+++ weber-integration/bin/controllers/controller1.dart
@@ -2,3 +2,6 @@ import 'package:weber/weber.dart';
 
 @Controller()
 class Controller1 {}
+
+final c = Controller1();
+final e = c.endpoint;
--- weber-integration/bin/server.dart
+++ weber-integration/bin/server.dart
@@ -3,6 +3,8 @@ import 'controllers/controller1.dart';
 
 void main(List<String> args) async {
   final server = WebServer();
+  final controller1 = Controller1();
+  final _ = controller1.endpoint;
 
   server.addController(Controller1());
   await server.start();
