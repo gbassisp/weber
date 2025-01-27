b0a7624d7b2589bd815c5fb6c8b3fd6185f8ae3f
guilherme <guilherme.bassi.sp@gmail.com>
2025-01-27 11:48:05 +1030

    feat: improved API

---
+++ weber-integration/bin/controllers/controller1.dart
@@ -0,0 +1,4 @@
+import 'package:weber/weber.dart';
+
+@Controller()
+class Controller1 {}
--- weber/lib/src/stable/weber_base.dart
+++ weber/lib/src/stable/weber_base.dart
@@ -1,2 +1,17 @@
+import 'package:weber/weber.dart';
+
 /// Checks if you are awesome. Spoiler: you are.
-class WebServer {}
+class WebServer {
+  /// Returns a [WebServer].
+  WebServer();
+
+  /// Adds a [controller] to the server.
+  void addController(ControllerBase controller) {
+    // print('Added controller: $controller');
+  }
+
+  /// Starts the server.
+  Future<void> start() async {
+    // print('Server started');
+  }
+}
